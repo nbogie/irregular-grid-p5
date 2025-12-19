1ac9c02d29934515f68c827aea5c637c4580f520
nbogie <neillbogie@googlemail.com>
2025-12-19 04:22:27 +0000

    wip

--- src/sketch.js
+++ src/sketch.js
@@ -5,6 +5,7 @@ let palette;
  * @typedef {Object} Palette
  * @property {string[]} allColours
  * @property {string} guidelines
+ * @property {string} placementOutline
  * @property {string} bg
  * @returns Palette
  */
@@ -21,9 +22,10 @@ function createPalette() {
   ];
 
   return {
-    allColours: colours,
+    allColours: colours.slice(0, 7),
     bg: colours[7],
     guidelines: colours[0],
+    placementOutline: colours.at(-1),
   };
 }
 /** @typedef {Object} Config
@@ -62,18 +64,29 @@ function draw() {
 
 /**
  * @param {Pos} pos
+ * @param {Placement[]} placements
  * @returns {boolean}
  */
 function isEmpty(pos, placements) {
-  return !placements.some((placement) =>
+  return emptyCheck(pos, placements).empty;
+}
+/**
+ * @param {Pos} pos
+ * @param {Placement[]} placements
+ * @returns {{empty: true} | {empty: false, firstCollision: Placement}}
+ */
+function emptyCheck(pos, placements) {
+  const firstCollision = placements.find((placement) =>
     placementCoversPosition(placement, pos)
   );
+  return firstCollision ? { empty: false, firstCollision } : { empty: true };
 }
+
 function createAndDrawIrregularGrid() {
   const allCellPositions = generateAllCellPositions();
   const placements = makePlacements(allCellPositions);
-  drawPlacements(placements);
   drawCellGuides();
+  drawPlacements(placements);
 }
 /**
  *
@@ -84,6 +97,7 @@ function drawPlacements(placements) {
 
   placements.forEach((pl) => {
     push();
+    stroke(palette.placementOutline);
     translate(pl.pos.x * cellSize, pl.pos.y * cellSize);
     fill(pl.colour);
     rect(0, 0, cellSize * pl.dims.w, cellSize * pl.dims.h);
@@ -92,7 +106,6 @@ function drawPlacements(placements) {
 }
 
 function drawCellGuides() {
-  console.log({ palette });
   const { cellSize } = config;
   for (let x = 0; x < width + cellSize; x += cellSize) {
     for (let y = 0; y < width + cellSize; y += cellSize) {
@@ -107,12 +120,13 @@ function drawCellGuides() {
  *
  * @param {Placement} placement
  * @param {Pos} targetPos
+ * @returns {boolean}
  */
 function placementCoversPosition(placement, targetPos) {
   const { x, y } = targetPos;
   const { x: px, y: py } = placement.pos;
   const { w, h } = placement.dims;
-  return x >= px && x <= px + w && y >= py && y <= py + h;
+  return x >= px && x < px + w && y >= py && y < py + h;
 }
 
 function mousePressed() {
@@ -132,27 +146,70 @@ function generateAllCellPositions() {
   }
   return posns;
 }
+
+/**
+ *
+ * @param {Pos} pos
+ * @param {Placement[]} pls
+ */
+function attemptPlacement(pos, pls) {
+  //TODO: consider whether this non 1x1 shape fits
+
+  const emptyResult = emptyCheck(pos, pls);
+  if (emptyResult.empty === true) {
+    const w = random([1, 2, 3]);
+    const h = random([1, 2, 3]);
+
+    /**@type {Placement} */
+    const placement = {
+      dims: { w, h },
+      pos: { x: pos.x, y: pos.y },
+      colour: random(palette.allColours),
+    };
+    pls.push(placement);
+    return { successful: true, data: placement };
+  } else {
+    return {
+      successful: false,
+      data: {
+        pos,
+        reason: "not empty",
+        firstCollision: emptyResult.firstCollision,
+      },
+    };
+  }
+}
+
 /**
  *
  * @param {Pos[]} allCellPositions
  * @return {Placement[]}
  */
 function makePlacements(allCellPositions) {
+  /** @type {Placement[]} */
   const pls = [];
-  allCellPositions.forEach((pos) => {
-    //TODO: consider whether this non 1x1 shape fits
-    if (isEmpty(pos, pls)) {
-      const w = random([1, 2, 3]);
-      const h = random([1, 2, 3]);
-      console.log(JSON.stringify({ w, h }));
-      /**@type {Placement} */
-      const placement = {
-        dims: { w, h },
-        pos: { x: pos.x, y: pos.y },
-        colour: random(palette.allColours),
-      };
-      pls.push(placement);
-    }
-  });
+
+  const results = allCellPositions.map((pos) => attemptPlacement(pos, pls));
+  const [_plsWrapped, skips] = partition(results, (r) => r.successful);
+  console.log({ skips });
   return pls;
+}
+
+/**
+ * @template T
+ * @template A
+ * @template B
+ * @param {T[]} arr
+ * @param {(el:T, ix?: number, arr?:T[]) => boolean} predFn
+ * @returns
+ */
+function partition(arr, predFn) {
+  const arrA = [];
+  const arrB = [];
+  for (let ix = 0; ix < arr.length; ix++) {
+    const element = arr[ix];
+    const target = predFn(element, ix, arr) ? arrA : arrB;
+    target.push(element);
+  }
+  return [arrA, arrB];
 }
