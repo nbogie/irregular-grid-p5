b09438aa834a528b8e151bfebf15f3806cf8aa25
nbogie <neillbogie@googlemail.com>
2025-12-19 05:41:08 +0000

    3d random building heights

--- src/example3d/sketch.js
+++ src/example3d/sketch.js
@@ -12,22 +12,26 @@ let placements;
 let config;
 
 /**
- * @typedef {Placement & { colour: string }} PlacementMore
+ * @typedef {Placement & { colour: string, heightScale: number }} PlacementMore
  */
 /** @typedef {Object} Config
  * @property {number} cellSize
  * @property {number} gap
+ * @property {boolean} enableStroke
  *
  */
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  const dim = min(windowWidth, windowHeight);
+  createCanvas(dim, dim, WEBGL);
   palette = createPalette();
-  config = { cellSize: width / 20, gap: 2 };
+  config = { cellSize: width / 20, gap: 10, enableStroke: false };
   placements = createIrregularGrid(config.cellSize).map((p) => ({
     ...p,
     colour: random(palette.allColours),
+    heightScale: random(0.5, 1),
   }));
+  setBodyBackgroundAsDarkerThan(palette.bg);
 }
 
 function draw() {
@@ -37,6 +41,10 @@ function draw() {
   drawPlacements3D(placements);
 }
 
+function setBodyBackgroundAsDarkerThan(baseColour) {
+  const lightnessFrac = 0.92;
+  document.body.style.backgroundColor = `oklch(from ${baseColour} calc(l * ${lightnessFrac}) c h)`;
+}
 /**
  *
  * @param {PlacementMore[]} placements
@@ -47,19 +55,24 @@ function drawPlacements3D(placements) {
 
   placements.forEach((pl) => {
     push();
-    noStroke();
-    // strokeWeight(3);
-    // stroke(palette.placementOutline);
+    if (config.enableStroke) {
+      strokeWeight(1);
+      stroke(palette.placementOutline);
+    } else {
+      noStroke();
+    }
+
     translate(pl.pos.x * cellSize, 0, pl.pos.y * cellSize);
     const c = color(pl.colour);
     const w = cellSize * pl.dims.w;
     const h = cellSize * pl.dims.h;
-    translate(w / 2, 0, h / 2);
+    const d = cellSize * 3 * pl.heightScale;
+    translate(w / 2, -d / 2, h / 2);
     if (frameCount === 1) {
       console.log(c);
     }
     ambientMaterial(c);
-    box(w - config.gap, 20, h - config.gap);
+    box(w - config.gap, d, h - config.gap);
     pop();
   });
 }
