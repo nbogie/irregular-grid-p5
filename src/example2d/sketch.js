027b3d20da8cc6102a74a4af1285161248aeab9d
nbogie <neillbogie@googlemail.com>
2025-12-19 06:37:11 +0000

    sketch includes its palette

--- src/example2d/sketch.js
+++ src/example2d/sketch.js
@@ -73,3 +73,31 @@ function drawCellGuidelines() {
 function mousePressed() {
   redraw();
 }
+
+/**
+ * @typedef {Object} Palette
+ * @property {string[]} allColours
+ * @property {string} guidelines
+ * @property {string} placementOutline
+ * @property {string} bg
+ * @returns Palette
+ */
+function createPalette() {
+  const colours = [
+    "#fef9c6",
+    "#ffcc4d",
+    "#f5b800",
+    "#56a1c4",
+    "#4464a1",
+    "#ee726b",
+    "#df5f50",
+    "#5a3034",
+  ];
+
+  return {
+    allColours: colours.slice(0, 7),
+    bg: colours[7],
+    guidelines: colours[0],
+    placementOutline: colours.at(-1),
+  };
+}
